aaabb44c17b450900ac1455569fbe7123cc02c3b
rMaloberti <r.maloberti@yahoo.com>
2023-06-05 15:46:35 +0200

    [docs] add comments to the app components

--- src/components/TransactionRow/index.jsx
+++ src/components/TransactionRow/index.jsx
@@ -1,3 +1,4 @@
+import { PropTypes } from 'prop-types';
 import { useEffect, useState } from 'react';
 import './index.css';
 import { formatMonth } from '../../utils/helpers';
@@ -7,10 +8,13 @@ import { useNavigate } from 'react-router';
 const TransactionRow = ({ transactionData, userBalance }) => {
   const { _id, amount, description, date } = transactionData;
 
+  /* React router navigate */
   const navigate = useNavigate();
 
+  /* React state to hold the formated version of the date */
   const [formatedDate, setFormatedDate] = useState(null);
 
+  /* Format the date as soon as we get it */
   useEffect(() => {
     const dateToFormat = new Date(date);
 
@@ -21,6 +25,10 @@ const TransactionRow = ({ transactionData, userBalance }) => {
     );
   }, [date]);
 
+  /**
+   * Navigate to the correct URL when clicking on a transaction.
+   * @function navigateToDetails
+   */
   const navigateToDetails = () => {
     navigate(`/transactions/${_id}`);
   };
@@ -41,3 +49,19 @@ const TransactionRow = ({ transactionData, userBalance }) => {
 };
 
 export default TransactionRow;
+
+TransactionRow.propTypes = {
+  transactionData: PropTypes.shape({
+    _id: PropTypes.string,
+    amount: PropTypes.number,
+    category: PropTypes.string,
+    createdAt: PropTypes.string,
+    date: PropTypes.string,
+    description: PropTypes.string,
+    notes: PropTypes.string,
+    transactionType: PropTypes.string,
+    updatedAt: PropTypes.string,
+    userId: PropTypes.string,
+  }),
+  userBalance: PropTypes.number,
+};
